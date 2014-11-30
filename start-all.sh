7e5e49dd72a016d4999494aa65a519e97eae6189
Keegan Bowen <keegan.bowen@gmail.com>
2014-11-30 03:08:35 -0700

    Update start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -7,7 +7,7 @@ echo "Starting up the Five Daemon MGMT group..."
 cd /var/tmp/cop-daemon/
 nohup /var/tmp/cop-daemon/cop-daemon.sh &
 cd /var/tmp/util-daemon/
-nohup /var/tmp/util-daemon/util-daemon.sh &
+nohup /var/tmp/util-daemon/util-daemon.sh /var/tmp/util-daemon/ 10 &
 cd /var/tmp/keeper-daemon/
 # The keeper daemon can have multiple configs, hadoop2 is just an example here.
 # Add your own configs and instances in!
