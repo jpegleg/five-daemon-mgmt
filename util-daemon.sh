782b84ef2e905eedb0a70f910f17fd633e356408
Keegan Bowen <keegan.bowen@gmail.com>
2014-11-30 02:39:19 -0700

    Update util-daemon.sh

--- util-daemon.sh
+++ util-daemon.sh
@@ -3,9 +3,10 @@
 # Utility Daemon by Keegan Bowen, 2014
 
 # Run like this:
-# nohup./util-daemon.sh &
+# nohup./util-daemon.sh /your/filesystem/ &
+UDIR="$1"
 
-mkdir -p /var/tmp/util-daemon/log
+mkdir -p $UDIR/log
 
 # Do reporting with these congfiguration files. 
 # They need to be in place.
@@ -20,70 +21,74 @@ mkdir -p /var/tmp/util-daemon/log
 # Edit line 74 to set the desired email address.
 
 # Alert for specific network connections
-touch /var/tmp/util-daemon/blacklist.conf
+touch $UDIR/blacklist.conf
 # Check on a network processes
-touch /var/tmp/util-daemon/netp.conf
+touch $UDIR/netp.conf
 # Look for a process to be stopped
-touch /var/tmp/util-daemon/killproc.conf
+touch $UDIR/killproc.conf
 # Check for a running proess
-touch /var/tmp/util-daemon/procp.conf
+touch $UDIR/procp.conf
 
 
 function gather () {
-    df -h > /var/tmp/util-daemon/df.out
-    uptime > /var/tmp/util-daemon/uptime.out
-    netstat -a | grep -v unix > /var/tmp/util-daemon/netstat.out
-    ps aux > /var/tmp/util-daemon/ps.out
+    df -h > $UDIR/df.out
+    uptime > $UDIR/uptime.out
+    netstat -a | grep -v unix > $UDIR/netstat.out
+    ps aux > $UDIR/ps.out
 }
 
 function report () {
-    grep [9][7-9]% /var/tmp/util-daemon/df.out > /var/tmp/util-daemon/log/df.warn
-    grep "100%" /var/tmp/util-daemon/df.out > /var/tmp/util-daemon/log/df.alert
-    grep keeper-daemon.sh /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/keeper.warn
-    grep install-daemon.sh /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/install.warn
-    grep mother-daemon.sh /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/mother.warn
-    grep util-daemon.sh /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/util.warn
-    grep cop-daemon.sh /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/cop.warn
+    grep [9][7-9]% $UDIR/df.out > $UDIR/log/df.warn
+    grep "100%" $UDIR/df.out > $UDIR/log/df.alert
+    grep keeper-daemon.sh $UDIR/ps.out > $UDIR/log/keeper.warn
+    grep install-daemon.sh $UDIR/ps.out > $UDIR/log/install.warn
+    grep mother-daemon.sh $UDIR/ps.out > $UDIR/log/mother.warn
+    grep util-daemon.sh $UDIR/ps.out > $UDIR/log/util.warn
+    grep cop-daemon.sh $UDIR/ps.out > $UDIR/log/cop.warn
     PROCLIM=$(cat /proc/cpuinfo | grep processor | wc -l)
-    grep [["$PROCLIM"-9]][0-9].[0-9][0-9] /var/tmp/util-daemon/uptime.out > /var/tmp/util-daemon/log/load.warn
-    grep [1-9][0-9][0-9].[0-9][0-9] /var/tmp/util-daemon/uptime.out > /var/tmp/util-daemon/log/load.alert
-    grep [1-9][0-9][0-9][0-9].[0-9][0-9] /var/tmp/util-daemon/uptime.out > /var/tmp/util-daemon/log/load2.alert
-    grep [1-9][0-9][0-9][0-9][0-9].[0-9][0-9] /var/tmp/util-daemon/uptime.out > /var/tmp/util-daemon/log/load3.alert
-    grep $(cat /var/tmp/util-daemon/blacklist.conf | cut -d',' -f1) /var/tmp/util-daemon/netstat.out > /var/tmp/util-daemon/log/blacklist.warn
-    grep $(cat /var/tmp/util-daemon/netp.conf | cut -d',' -f1) /var/tmp/util-daemon/netstat.out > /var/tmp/util-daemon/log/netp.log
-    grep $(cat /var/tmp/util-daemon/killproc.conf | cut -d',' -f1) /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/killproc.warn
-    grep $(cat /var/tmp/util-daemon/procp.conf | cut -d',' -f1) /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/procp.log
-    grep $(cat /var/tmp/util-daemon/blacklist.conf | cut -d',' -f2) /var/tmp/util-daemon/netstat.out > /var/tmp/util-daemon/log/blacklist2.warn
-    grep $(cat /var/tmp/util-daemon/netp.conf | cut -d',' -f2) /var/tmp/util-daemon/netstat.out > /var/tmp/util-daemon/log/netp2.log
-    grep $(cat /var/tmp/util-daemon/killproc.conf | cut -d',' -f2) /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/killproc2.warn
-    grep $(cat /var/tmp/util-daemon/procp.conf | cut -d',' -f2) /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/procp2.log
-    grep $(cat /var/tmp/util-daemon/blacklist.conf | cut -d',' -f3) /var/tmp/util-daemon/netstat.out > /var/tmp/util-daemon/log/blacklist3.warn
-    grep $(cat /var/tmp/util-daemon/netp.conf | cut -d',' -f3) /var/tmp/util-daemon/netstat.out > /var/tmp/util-daemon/log/netp3.log
-    grep $(cat /var/tmp/util-daemon/killproc.conf | cut -d',' -f3) /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/killproc3.warn
-    grep $(cat /var/tmp/util-daemon/procp.conf | cut -d',' -f3) /var/tmp/util-daemon/ps.out > /var/tmp/util-daemon/log/procp3.log
+    grep [["$PROCLIM"-9]][0-9].[0-9][0-9] $UDIR/uptime.out > $UDIR/log/load.warn
+    grep [1-9][0-9][0-9].[0-9][0-9] $UDIR/uptime.out > $UDIR/log/load.alert
+    grep [1-9][0-9][0-9][0-9].[0-9][0-9] $UDIR/uptime.out > $UDIR/log/load2.alert
+    grep [1-9][0-9][0-9][0-9][0-9].[0-9][0-9] $UDIR/uptime.out > $UDIR/log/load3.alert
+    grep $(cat $UDIR/blacklist.conf | cut -d',' -f1) $UDIR/netstat.out > $UDIR/log/blacklist.warn
+    grep $(cat $UDIR/netp.conf | cut -d',' -f1) $UDIR/netstat.out > $UDIR/log/netp.log
+    grep $(cat $UDIR/killproc.conf | cut -d',' -f1) $UDIR/ps.out > $UDIR/log/killproc.warn
+    grep $(cat $UDIR/procp.conf | cut -d',' -f1) $UDIR/ps.out > $UDIR/log/procp.log
+    grep $(cat $UDIR/blacklist.conf | cut -d',' -f2) $UDIR/netstat.out > $UDIR/log/blacklist2.warn
+    grep $(cat $UDIR/netp.conf | cut -d',' -f2) $UDIR/netstat.out > $UDIR/log/netp2.log
+    grep $(cat $UDIR/killproc.conf | cut -d',' -f1) $UDIR/ps.out > $UDIR/log/killproc.warn
+    grep $(cat $UDIR/procp.conf | cut -d',' -f1) $UDIR/ps.out > $UDIR/log/procp.log
+    grep $(cat $UDIR/blacklist.conf | cut -d',' -f2) $UDIR/netstat.out > $UDIR/log/blacklist2.warn
+    grep $(cat $UDIR/netp.conf | cut -d',' -f2) $UDIR/netstat.out > $UDIR/log/netp2.log
+    grep $(cat $UDIR/killproc.conf | cut -d',' -f2) $UDIR/ps.out > $UDIR/log/killproc2.warn
+    grep $(cat $UDIR/procp.conf | cut -d',' -f2) $UDIR/ps.out > $UDIR/log/procp2.log
+    grep $(cat $UDIR/blacklist.conf | cut -d',' -f3) $UDIR/netstat.out > $UDIR/log/blacklist3.warn
+    grep $(cat $UDIR/netp.conf | cut -d',' -f3) $UDIR/netstat.out > $UDIR/log/netp3.log
+    grep $(cat $UDIR/killproc.conf | cut -d',' -f3) $UDIR/ps.out > $UDIR/log/killproc3.warn
+    grep $(cat $UDIR/procp.conf | cut -d',' -f3) $UDIR/ps.out > $UDIR/log/procp3.log
 }
 
 function email () {
-    echo "Utility Daemon Report $(date) from $(uname -a)" > /var/tmp/util-daemon/logreport
-    cat /var/tmp/util-daemon/log/* >> /var/tmp/util-daemon/logreport
+    echo "Utility Daemon Report $(date) from $(uname -a)" > $UDIR/logreport
+    cat $UDIR/log/* >> $UDIR/logreport
 #
 #          SET THE EMAIL BY EDITING THE SCRIPT HERE--------------------------------------------------
 #                                                                                                   |
 #                                                                                                  \ /
 #                                                                                                   V
-    echo "$(cat /var/tmp/util-daemon/logreport)" | mailx -s "Utility Daemon Report $(date)" example@someplace.com
+    echo "$(cat $UDIR/logreport)" | mailx -s "Utility Daemon Report $(date)" example@someplace.com
 }
 
-touch /var/tmp/util-daemon/email.send
+touch $UDIR/email.send
 
 # If there is anything in email.send, emails will be sent continously! 
 # It can be a lot of emails. Use with caution.
 # Example way to use the email.send file:
-# echo 1 > /var/tmp/util-daemon/email.send && sleep 300 && cp /dev/null /var/tmp/util-daemon/email.send
+# echo 1 > $UDIR/email.send && sleep 300 && cp /dev/null $UDIRemail.send
 # That sends emails for five minutes then stops them. Something like that could be used in a cron entry etc.
 
 function checkemail () {
-if [ -s /var/tmp/util-daemon/email.send ]
+if [ -s $UDIR/email.send ]
 then
      echo "EMAILING REPORT"
      email
@@ -97,7 +102,7 @@ while true; do
     gather
     report & # If one of the greps hangs, the daemon just moves on...
     checkemail
-    sleep $(cat /var/tmp/util-daemon/util.sleep)
+    sleep $(cat $UDIR/util.sleep)
 done
 
-# Set the sleep amount in seconds by placing a number in /var/tmp/util-daemon/util.sleep
+# Set the sleep amount in seconds by placing a number in $UDIR/util.sleep
